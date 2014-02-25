4fc62fbc8721cb14068719c8ba55471a4bdf1ce3
Matt Wright <matt@nobien.net>
2014-02-25 00:36:33 -0500

    Polish

--- test/cookbooks/stackdriver_test/recipes/lwrp.rb
+++ test/cookbooks/stackdriver_test/recipes/lwrp.rb
@@ -1,6 +1,6 @@
 
-# include_recipe "memcached"
-# include_recipe "stackdriver::default"
+include_recipe "memcached"
+include_recipe "stackdriver::default"
 
 stackdriver_plugin "memcached" do
   settings({:host => 'localhost', :port => 11211})
